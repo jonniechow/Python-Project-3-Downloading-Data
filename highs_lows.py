15c7850e8065abb44b2817d6822fec282c0e7505
Jonny Chow <Jonny@MacBook-Pro-8.local>
2018-06-07 13:37:04 -0400

    Included error checking for missing data through try-error catch block.

--- highs_lows.py
+++ highs_lows.py
@@ -1,27 +1,38 @@
 import csv
+from datetime import datetime
 
 from matplotlib import pyplot as plt 
 
-# Get high temperatures from file.
-filename = 'sitka_weather_07-2014.csv'
+# Get dates, high, and low temperatures from file.
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    highs = []
+    dates, highs, lows = [], [], []
     for row in reader:
-        high = int(row[1])
-        highs.append(row[1])
-
-    print(highs)
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot data.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2014", fontsize=14)
+title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=20)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
